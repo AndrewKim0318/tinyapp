a6474ac235609411c07854d3d3df44787d0e079c
Andrew Kim <wk@ualberta.ca>
2019-08-05 19:58:42 +0000

    added html to /hello path

--- express_server.js
+++ express_server.js
@@ -18,3 +18,8 @@ app.listen(port, () => {
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+

ea9eacc2038335e6d5ec47a2713c559ea7017c3a
Andrew Kim <wk@ualberta.ca>
2019-08-06 14:54:21 +0000

    added a delete button next to the urls and delete the url once the button is click

--- express_server.js
+++ express_server.js
@@ -65,8 +65,13 @@ app.post('/urls', (req, res) => {
   res.render(`url_show`, templateVars);
   res.redirect(`/urls/${shortURL}`);
 });
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
 
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
